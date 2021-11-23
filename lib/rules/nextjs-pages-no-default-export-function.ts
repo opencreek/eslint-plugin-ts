e8afc4ea843021820ecaef84b9bc9526b85da2c5
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2021-11-23 18:50:47 +0100

    :rotating_light:

--- lib/rules/nextjs-pages-no-default-export-function.ts
+++ lib/rules/nextjs-pages-no-default-export-function.ts
@@ -1,11 +1,12 @@
-import {RuleCreator} from "@typescript-eslint/experimental-utils/dist/eslint-utils"
-import path from "path"
+import { RuleCreator } from "@typescript-eslint/experimental-utils/dist/eslint-utils"
 import type {
     AssignmentExpression,
     ExpressionStatement,
-    FunctionDeclaration, Identifier, MemberExpression,
-    Program
-} from "@typescript-eslint/types/dist/ast-spec";
+    FunctionDeclaration,
+    Identifier,
+    MemberExpression,
+    Program,
+} from "@typescript-eslint/types/dist/ast-spec"
 
 const creator = RuleCreator((rule) => rule)
 
@@ -21,40 +22,48 @@ export default creator<Options, MessageIds>({
         },
         fixable: "code",
         messages: {
-            "no-default-export-function": "No 'default export function', because of react docgen imports.",
+            "no-default-export-function":
+                "No 'default export function', because of react docgen imports.",
         },
-        schema: [
-            {},
-        ],
+        schema: [{}],
     },
     defaultOptions: [],
     create(context) {
         return {
             ExportDefaultDeclaration(node) {
                 if (node.declaration.type === "FunctionDeclaration") {
-                    const functionDeclaration = node.declaration as FunctionDeclaration
+                    const functionDeclaration =
+                        node.declaration as FunctionDeclaration
                     // check if we are a page
 
                     const fileName = context.getPhysicalFilename?.()
                     if (fileName?.includes("pages/")) {
                         const parent = node.parent as Program
-                        if (parent == null) throw new Error("Can not resolve parent")
-                        const assignmentToDefault = parent.body.some(it => {
-                            if (it.type !== "ExpressionStatement" || (it as ExpressionStatement).expression.type !== "AssignmentExpression") {
+                        if (parent == null)
+                            throw new Error("Can not resolve parent")
+                        const assignmentToDefault = parent.body.some((it) => {
+                            if (
+                                it.type !== "ExpressionStatement" ||
+                                (it as ExpressionStatement).expression.type !==
+                                    "AssignmentExpression"
+                            ) {
                                 return false
                             }
 
-                            const expression = it.expression as AssignmentExpression
+                            const expression =
+                                it.expression as AssignmentExpression
                             if (expression.left.type !== "MemberExpression") {
                                 return
                             }
                             const left = expression.left as MemberExpression
                             if (left.object.type !== "Identifier") {
-                               return
+                                return
                             }
 
                             const identifier = left.object as Identifier
-                            return functionDeclaration.id?.name === identifier.name
+                            return (
+                                functionDeclaration.id?.name === identifier.name
+                            )
                         })
 
                         // The bug is not happening
@@ -67,23 +76,22 @@ export default creator<Options, MessageIds>({
                             messageId: "no-default-export-function",
                             data: {},
                             fix: (fixer) => {
-
                                 return [
                                     fixer.removeRange([
                                         node.range[0],
-                                        node.range[0] + "export default ".length
+                                        node.range[0] +
+                                            "export default ".length,
                                     ]),
-                                    fixer.insertTextAfter(parent, `\nexport default ${functionDeclaration?.id?.name}`)
+                                    fixer.insertTextAfter(
+                                        parent,
+                                        `\nexport default ${functionDeclaration?.id?.name}`
+                                    ),
                                 ]
-
                             },
                         })
                     }
-
                 }
-
-            }
-
+            },
         }
     },
 })
--- tests/lib/rules/nextjs-pages-no-default-export-function.test.ts
+++ tests/lib/rules/nextjs-pages-no-default-export-function.test.ts
@@ -12,35 +12,39 @@ import process from "process"
 const spy = jest.spyOn(process, "cwd")
 spy.mockReturnValue("/")
 
-ruleTester.run("nextjs-no-default-export-function", nextjsNoDefaultExportFunction, {
-    valid: [
-        {
-            code: 'export default function Test() {}\nTest.layout = someLayout',
-            filename: "src/nested/deep/test.js",
-        },
-        {
-            code: 'export default function Test() {}',
-            filename: "src/nested/deep/test.js",
-        },
-        {
-            code: 'function Test() {}\nexport default Test',
-            filename: "src/pages/nested/deep/test.js",
-        },
-        {
-            code: 'export default function Test() {}',
-            filename: "pages/nested/deep/test.js",
-        },
-    ],
-    invalid: [
-        {
-            code: 'export default function Test() {}\nTest.Layout = someLayout',
-            filename: "src/pages/else/test.js",
-            errors: [
-                {
-                    messageId: "no-default-export-function",
-                },
-            ],
-            output: 'function Test() {}\nTest.Layout = someLayout\nexport default Test',
-        },
-    ],
-})
+ruleTester.run(
+    "nextjs-no-default-export-function",
+    nextjsNoDefaultExportFunction,
+    {
+        valid: [
+            {
+                code: "export default function Test() {}\nTest.layout = someLayout",
+                filename: "src/nested/deep/test.js",
+            },
+            {
+                code: "export default function Test() {}",
+                filename: "src/nested/deep/test.js",
+            },
+            {
+                code: "function Test() {}\nexport default Test",
+                filename: "src/pages/nested/deep/test.js",
+            },
+            {
+                code: "export default function Test() {}",
+                filename: "pages/nested/deep/test.js",
+            },
+        ],
+        invalid: [
+            {
+                code: "export default function Test() {}\nTest.Layout = someLayout",
+                filename: "src/pages/else/test.js",
+                errors: [
+                    {
+                        messageId: "no-default-export-function",
+                    },
+                ],
+                output: "function Test() {}\nTest.Layout = someLayout\nexport default Test",
+            },
+        ],
+    }
+)
