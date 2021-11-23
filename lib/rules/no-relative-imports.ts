7c5eda76f3c8064907eba4c79b196d9f91e05cf5
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2021-11-23 09:23:30 +0100

    :bug: :rotating_light:

--- lib/rules/no-relative-imports.ts
+++ lib/rules/no-relative-imports.ts
@@ -1,72 +1,82 @@
-import { RuleCreator } from "@typescript-eslint/experimental-utils/dist/eslint-utils";
-import path from "path";
+import { RuleCreator } from "@typescript-eslint/experimental-utils/dist/eslint-utils"
+import path from "path"
 
-const creator = RuleCreator((rule) => rule);
+const creator = RuleCreator((rule) => rule)
 
 export type Options = {
-  baseUrl?: string;
-}[];
-export type MessageIds = "standard-message";
+    baseUrl?: string
+}[]
+export type MessageIds = "standard-message"
 export default creator<Options, MessageIds>({
-  name: "no-relative-imports",
-  meta: {
-    type: "problem",
-    docs: {
-      description: "Requires using non-relative imports with baseUrl",
-      recommended: "error",
-    },
-    fixable: "code",
-    messages: {
-      "standard-message": "No relative imports, that go to back to the baseURl",
-    },
-    schema: [
-      {
-        type: "object",
-        properties: {
-          baseUrl: {
-            type: "string",
-          },
+    name: "no-relative-imports",
+    meta: {
+        type: "problem",
+        docs: {
+            description: "Requires using non-relative imports with baseUrl",
+            recommended: "error",
         },
-      },
-    ],
-  },
-  defaultOptions: [],
-  create(context) {
-    return {
-      ImportDeclaration(node) {
-        const fileName = context.getPhysicalFilename?.();
-        if (fileName == undefined) {
-          console.error("Got no physical file name ?!");
-          return;
-        }
-
-        const basePath = path.resolve(
-          process.cwd(),
-          context.options?.[0]?.baseUrl ?? "."
-        );
-        const relativeFileName = fileName.replace(basePath, "");
-        const levels = relativeFileName.split("/").length - 2;
-        const levelImport = "../".repeat(levels);
-
-        if (node.source.value.startsWith(levelImport)) {
-          const withoutLevels = node.source.value.replace(levelImport, "");
-
-          // we go behond the baseURl
-          if (withoutLevels.startsWith("..")) return;
-
-          context.report({
-            node: node.source,
-            messageId: "standard-message",
-            data: {},
-            fix: (fixer) => {
-              return fixer.replaceText(
-                node.source,
-                '"' + node.source.value.replace(levelImport, "") + '"'
-              );
+        fixable: "code",
+        messages: {
+            "standard-message":
+                "No relative imports, that go to back to the baseURl",
+        },
+        schema: [
+            {
+                type: "object",
+                properties: {
+                    baseUrl: {
+                        type: "string",
+                    },
+                },
             },
-          });
+        ],
+    },
+    defaultOptions: [],
+    create(context) {
+        return {
+            ImportDeclaration(node) {
+                const fileName = context.getPhysicalFilename?.()
+                if (fileName == undefined) {
+                    console.error("Got no physical file name ?!")
+                    return
+                }
+
+                const basePath = path.resolve(
+                    process.cwd(),
+                    context.options?.[0]?.baseUrl ?? "."
+                )
+
+                if (!fileName.startsWith(basePath)) {
+                    return
+                }
+                const relativeFileName = fileName.replace(basePath, "")
+                const levels = relativeFileName.split("/").length - 2
+                const levelImport = "../".repeat(levels)
+
+                if (node.source.value.startsWith(levelImport)) {
+                    const withoutLevels = node.source.value.replace(
+                        levelImport,
+                        ""
+                    )
+
+                    // we go behond the baseURl
+                    if (withoutLevels.startsWith("..")) return
+
+                    context.report({
+                        node: node.source,
+                        messageId: "standard-message",
+                        data: {},
+                        fix: (fixer) => {
+                            return fixer.replaceText(
+                                node.source,
+                                '"' +
+                                    node.source.value.replace(levelImport, "") +
+                                    '"'
+                            )
+                        },
+                    })
+                }
+            },
         }
-      },
-    };
-  },
-});
+    },
+})
--- tests/lib/rules/no-relative-imports.test.ts
+++ tests/lib/rules/no-relative-imports.test.ts
@@ -1,46 +1,51 @@
-import noRelativeImports from "../../../lib/rules/no-relative-imports";
-import { ESLintUtils } from "@typescript-eslint/experimental-utils";
+import noRelativeImports from "../../../lib/rules/no-relative-imports"
+import { ESLintUtils } from "@typescript-eslint/experimental-utils"
 
-const RuleTester = ESLintUtils.RuleTester;
+const RuleTester = ESLintUtils.RuleTester
 
 const ruleTester = new RuleTester({
-  parser: "@typescript-eslint/parser",
-});
+    parser: "@typescript-eslint/parser",
+})
 
-import process from "process";
+import process from "process"
 
-const spy = jest.spyOn(process, "cwd");
-spy.mockReturnValue("/");
+const spy = jest.spyOn(process, "cwd")
+spy.mockReturnValue("/")
 
 ruleTester.run("my-rule", noRelativeImports, {
-  invalid: [
-    {
-      options: [{ baseUrl: "./src" }],
-      code: 'import {foo} from "../../bla/test"',
-      filename: "/src/nested/deep/test.js",
-      errors: [
+    valid: [
         {
-          messageId: "standard-message",
+            options: [{ baseUrl: "./src" }],
+            code: 'import {foo} from "bla/test"',
+            filename: "/src/nested/deep/test.js",
         },
-      ],
-      output: 'import {foo} from "bla/test"',
-    },
-  ],
-  valid: [
-    {
-      options: [{ baseUrl: "./src" }],
-      code: 'import {foo} from "bla/test"',
-      filename: "/src/nested/deep/test.js",
-    },
-    {
-      options: [{ baseUrl: "./src" }],
-      code: 'import {foo} from "../../../bla/test"',
-      filename: "/src/nested/deep/test.js",
-    },
-    {
-      options: [{ baseUrl: "./src" }],
-      code: 'import {foo} from "../bla/test"',
-      filename: "/src/nested/deep/test.js",
-    },
-  ],
-});
+        {
+            options: [{ baseUrl: "./src" }],
+            code: 'import {foo} from "../../../bla/test"',
+            filename: "/src/nested/deep/test.js",
+        },
+        {
+            options: [{ baseUrl: "./src" }],
+            code: 'import {foo} from "../bla/test"',
+            filename: "/src/nested/deep/test.js",
+        },
+        {
+            options: [{ baseUrl: "./src" }],
+            code: 'import {foo} from "../../../bla/test"',
+            filename: "/test/nested/deep/test.js",
+        },
+    ],
+    invalid: [
+        {
+            options: [{ baseUrl: "./src" }],
+            code: 'import {foo} from "../../bla/test"',
+            filename: "/src/nested/deep/test.js",
+            errors: [
+                {
+                    messageId: "standard-message",
+                },
+            ],
+            output: 'import {foo} from "bla/test"',
+        },
+    ],
+})
